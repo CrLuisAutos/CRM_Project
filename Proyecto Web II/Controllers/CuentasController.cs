4146ece1daa164b0ee6b70c6a12dd334074cd84b
Luis Espinoza <espinozaluis3680@gmail.com>
2017-06-25 07:24:53 -0600

    Create a project

---
+++ Proyecto Web II/Controllers/CuentasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Web_II.Models;
+
+namespace Proyecto_Web_II.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Cuentas
+        public ActionResult Index()
+        {
+            return View(db.Cuentas.ToList());
+        }
+
+        // GET: Cuentas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cuenta cuenta = db.Cuentas.Find(id);
+            if (cuenta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cuenta);
+        }
+
+        // GET: Cuentas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cuentas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Cedula_Juridica,Pagina_Web,Direccion_Fisica,Numero_de_telefono,Sector")] Cuenta cuenta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cuentas.Add(cuenta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cuenta);
+        }
+
+        // GET: Cuentas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cuenta cuenta = db.Cuentas.Find(id);
+            if (cuenta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cuenta);
+        }
+
+        // POST: Cuentas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Cedula_Juridica,Pagina_Web,Direccion_Fisica,Numero_de_telefono,Sector")] Cuenta cuenta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cuenta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cuenta);
+        }
+
+        // GET: Cuentas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cuenta cuenta = db.Cuentas.Find(id);
+            if (cuenta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cuenta);
+        }
+
+        // POST: Cuentas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cuenta cuenta = db.Cuentas.Find(id);
+            db.Cuentas.Remove(cuenta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proyecto Web II/Models/Cuenta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Web_II.Models
+{
+    public class Cuenta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        [DisplayName("Nombre")]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = "El nombre debe tener una longitud entre 2 y 10 caracteres")]
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        [DisplayName("Cédula Jurídica")]
+        public int Cedula_Juridica { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        [DisplayName("Página Web")]
+        public String Pagina_Web { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        [DisplayName("Dirección Física")]
+        [StringLength(150, MinimumLength = 10, ErrorMessage = "La dirección física debe tener una longitud 10 y 150 caracteres")]
+        public String Direccion_Fisica { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        [DisplayName("Número de Teléfono")]
+        public int Numero_de_telefono { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public SectorList Sector { get; set; }
+
+    }
+    public enum SectorList
+    {
+        Educación,
+        Industria,
+        Agricola,
+        Manufactura,
+        Servicios
+    }
+}
